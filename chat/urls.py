2cbdcb4781ed48d47d46581fde2b510d8113f790
alireza mehraban <erfanm1282@gmail.com>
2024-12-13 11:53:26 +0330

    13-12-2024

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
+from .views import MessageHistoryView
 
-
-# urlpatterns = [
-    
-# ]
-
+urlpatterns = [
+    path('api/messages/<str:room_name>/', MessageHistoryView.as_view(), name='message_history'),
+]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Message
+from .serializers import MessageSerializer
 
-# Create your views here.
+class MessageHistoryView(APIView):
+    def get(self, request, room_name):
+        messages = Message.objects.filter(room_name=room_name).order_by('timestamp')
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
