96d1e0fc816e1663aa7900108d3295107a57ff50
alireza mehraban <erfanm1282@gmail.com>
2024-12-16 16:20:59 +0330

    change database

--- GiahPezeshk/settings.py
+++ GiahPezeshk/settings.py
@@ -81,12 +81,25 @@ WSGI_APPLICATION = 'GiahPezeshk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'plant',  # The name of your MongoDB database
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb://127.0.0.1:27017/plant',  # Connection string to your MongoDB database
+        }
     }
 }
+
+
 
 
 # Password validation
